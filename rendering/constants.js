87bb59ebe05b873c1dceba85a447f9ad59ab32f1
Hana Lee <leehana@alumni.stanford.edu>
2022-08-10 16:12:56 -0700

    Modify render constants to allow >4 cards to be rendered

--- rendering/constants.js
+++ rendering/constants.js
@@ -1,6 +1,10 @@
 export const CardFront = ["card-front.html", { width: 2035, height: 750 }];
+export const CardFront1 = ["card-front-1.html", { width: 2035, height: 750 }];
+export const CardFront2 = ["card-front-2.html", { width: 2035, height: 750 }];
 export const CardBack = ["card-back.html", { width: 2035, height: 750 }];
+export const CardBack1 = ["card-back-1.html", { width: 2035, height: 750 }];
+export const CardBack2 = ["card-back-2.html", { width: 2035, height: 750 }];
 export const BoardFront = ["board-front.html", { width: 1827, height: 1237 }];
 export const BoardLore = ["board-lore.html", { width: 1827, height: 1237 }];
 export const BoardRules = ["board-rules.html", { width: 1827, height: 1237 }];
-export const SourceFiles = [CardFront, CardBack, BoardFront, BoardLore, BoardRules];
+export const SourceFiles = [CardFront, CardBack, BoardFront, BoardLore, BoardRules, CardFront1, CardFront2, CardBack1, CardBack2];
