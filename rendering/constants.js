fc4fc8f55e93398298d84d8506cbf44c89b018d7
gcole <gcole@palantir.com>
2022-12-05 17:17:41 -0800

    New adversaries, and rendering support for them.

--- rendering/constants.js
+++ rendering/constants.js
@@ -1,21 +1,27 @@
 import path from "path";
 import * as url from "url";
 
-export const SpiritGroups = ["greg-spirits", "shalin-hana-spirit"];
+export const SpiritGroups = ["greg-spirits", "shalin-hana-spirit", "greg-adversaries"];
 
 const CardScale = 0.97;
 const BoardScale = 0.88;
 
-export const CardFront = ["card-front.html", { width: 2035, height: 750 }, CardScale];
-export const CardFront1 = ["card-front-1.html", { width: 2035, height: 750 }, CardScale];
-export const CardFront2 = ["card-front-2.html", { width: 2035, height: 750 }, CardScale];
-export const CardBack = ["card-back.html", { width: 2035, height: 750 }, CardScale];
-export const CardBack1 = ["card-back-1.html", { width: 2035, height: 750 }, CardScale];
-export const CardBack2 = ["card-back-2.html", { width: 2035, height: 750 }, CardScale];
-export const BoardFront = ["board-front.html", { width: 1827, height: 1237 }, BoardScale];
-export const BoardLore = ["board-lore.html", { width: 1827, height: 1237 }, BoardScale];
-export const BoardRules = ["board-rules.html", { width: 1827, height: 1237 }, BoardScale];
-export const SourceFiles = [CardFront, CardBack, BoardFront, BoardLore, BoardRules, CardFront1, CardFront2, CardBack1, CardBack2];
+const CardSize = { width: 2035, height: 750, deviceScaleFactor: 2 };
+const BoardSize = { width: 1827, height: 1237, deviceScaleFactor: 2 };
+const AdversarySize = { width: 1000, height: 667, deviceScaleFactor: 4 };
+
+export const CardFront = ["card-front.html", CardSize, CardScale];
+export const CardFront1 = ["card-front-1.html", CardSize, CardScale];
+export const CardFront2 = ["card-front-2.html", CardSize, CardScale];
+export const CardBack = ["card-back.html", CardSize, CardScale];
+export const CardBack1 = ["card-back-1.html", CardSize, CardScale];
+export const CardBack2 = ["card-back-2.html", CardSize, CardScale];
+export const BoardFront = ["board-front.html", BoardSize, BoardScale];
+export const BoardLore = ["board-lore.html", BoardSize, BoardScale];
+export const BoardRules = ["board-rules.html", BoardSize, BoardScale];
+export const Adversary = ["adversary.html", AdversarySize, BoardScale];
+export const AdversaryExtra = ["adversary-extra.html", AdversarySize, BoardScale];
+export const SourceFiles = [CardFront, CardBack, BoardFront, BoardLore, BoardRules, CardFront1, CardFront2, CardBack1, CardBack2, Adversary, AdversaryExtra];
 
 export const ScriptDir = url.fileURLToPath(new URL(".", import.meta.url));
 export const ProjectDir = path.resolve(ScriptDir, `../`);
